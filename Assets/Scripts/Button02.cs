b3c6112390a7be98c69798b598e0d5383f3ceaeb
JukovYuri <jukov_yuri@mail.ru>
2021-02-11 18:22:56 +0300

    11-02

---
+++ Assets/Scripts/Button02.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button02 : MonoBehaviour
+{
+	public UnityEvent OnPressed;
+	public UnityEvent OnUnPressed;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		OnPressed.Invoke();
+		print("qwert");
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		OnUnPressed.Invoke();
+	}
+}
+
+
---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+public class DoorController : MonoBehaviour
+{
+
+	[SerializeField] private float speed;
+	private bool goingUp = true;
+	private bool isActivate;
+	Animator animator;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+
+	public void ActivateUp()
+	{
+		//StopAllCoroutines();
+		//StartCoroutine(DoorUp());
+		animator.SetTrigger("Up");
+	}
+
+	public void ActivateDown()
+	{
+		//StopAllCoroutines();
+		//StartCoroutine(DoorDown());
+		animator.SetTrigger("Down");
+	}
+
+	IEnumerator DoorUp()
+	{
+		while (transform.position.y < 10)
+		{
+			transform.position += Vector3.up * speed * Time.deltaTime;
+			yield return null;
+		}
+	}
+
+	IEnumerator DoorDown()
+	{
+		while (transform.position.y > 2)
+		{
+			transform.position -= Vector3.up * speed * Time.deltaTime;
+			yield return null;
+		}
+	}
+}
